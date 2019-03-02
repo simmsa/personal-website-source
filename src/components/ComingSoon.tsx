52cd4f02d27cab9e1fc1ddd185cce4d1db872a1a
Andrew Simms <simms.andrew@gmail.com>
2019-03-01 23:58:07 -0700

    Page: Add coming soon page

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import Layout from "./Layout";
+
+const ComingSoon = () => (
+  <Layout title="Coming Soon">
+    <h1>Coming Soon...</h1>
+  </Layout>
+);
+
+export default ComingSoon;

de1d1283d752c6a2a02f2ffdea8af73d65bfc5fa
Andrew Simms <simms.andrew@gmail.com>
2019-03-08 22:53:51 -0700

    Feature: Show blog posts on the front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
+import BlogPosts from "../components/BlogPosts";
 import Layout from "../components/Layout";
 
 import Portrait from "../images/Andrew-Simms.jpg";
@@ -18,10 +19,12 @@ const IndexPage = () => (
         style={{ maxWidth: "250px" }}
       />
     </Container>
-    <p className="h3">
+    <p className="h3 mb-3">
       I am a programmer, photographer, guitar player, and maker currently living
       in Golden, Colorado.
     </p>
+    <h3 className="pt-1 mt-5 mb-4">Newest Blog Posts</h3>
+    <BlogPosts titleSize={4} />
   </Layout>
 );
 
